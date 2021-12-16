630646fa8c429291074851e14160babc8776b794
loonytoony0 <yakrobat@protonmail.com>
2021-12-16 17:04:45 +0500

    Add Russian translations

---
+++ l10n/ru.js
@@ -0,0 +1,25 @@
+OC.L10N.register(
+    "video_converter",
+    {
+        "Convert into" : "Конвертировать в",
+        "Note: This could take a considerable amount of time depending on your hardware and the preset you chose. You can safely close this window." : "Примечание: Эта операция может занять значительное количество времени в зависимости от ваших аппаратных средств и выбранного профиля.",
+        "Preset" : "Профиль",
+        "default" : "по умолчанию",
+        "Note: faster means worse quality or bigger size." : "Примечание: чем быстрее профиль - тем хуже качество/больше размер файла.",
+        "Priority" : "Приоритет",
+        "High" : "Высокий",
+        "Normal" : "Средний",
+        "Low" : "Низкий",
+        "Codec" : "Кодек",
+        "Auto" : "Авто",
+        "Target bitrate" : "Целевой битрейт",
+        "Mbit" : "Мбит",
+        "Mbit/s" : "Мбит/с", 
+        "Scale to" : "Масштаб",
+        "Keep" : "Тот же",
+        "Keep aspect" : "Сохранять соотношение сторон",
+        "Use faststart (for MP4)" : "Использовать faststart (для MP4)",
+        "Choose the output format" : "Выберите формат вывода"
+    },
+    "nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);"
+);
